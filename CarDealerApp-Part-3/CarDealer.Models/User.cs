64d16ed0702b55f41a868b14059e00552cf7111f
Petar Dimitrov <lbmikesmith@gmail.com>
2017-03-15 13:20:33 +0200

    Add files via upload

---
+++ CarDealerApp-Part-3/CarDealer.Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace CarDealer.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9]{5,}$")]
+        public string Username { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9]+@[a-z]+\.[a-z]+$")]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ CarDealerApp-Part-3/CarDealerApp/Security/SignInManager.cs
@@ -0,0 +1,25 @@
+﻿namespace CarDealerApp.Security
+{
+    using System.Linq;
+    using CarDealer.Data;
+    using CarDealer.Models;
+
+    public static class SignInManager
+    {
+        public static bool IsAuthenticated(string sessionId, CarDealerContext context)
+        {
+            return context.Sessions.Any(s => s.SessionId == sessionId && s.IsActive);
+        }
+
+        public static void Logout(string sessionId, CarDealerContext context)
+        {
+            context.Sessions.FirstOrDefault(s => s.SessionId == sessionId).IsActive = false;
+            context.SaveChanges();
+        }
+
+        public static User GetAuthenticatedUser(string sessionId, CarDealerContext context)
+        {
+            return context.Sessions.FirstOrDefault(s => s.SessionId == sessionId && s.IsActive)?.User;
+        }
+    }
+}
